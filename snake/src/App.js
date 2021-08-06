391584bd4912907865bc2c8e2817d31307a3d351
alec-deng <alexdeng901@gmail.com>
2021-08-06 16:35:52 -0400

    start

--- snake/src/App.js
+++ snake/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import Grid from './Grid.js';
-import './App.css';
 
-function App() {
+class App extends Component {
+
+  render() {
+    return (
+      <div className="board">
+        <div className="snake" style={{top:0, left:0}}></div>
+        <div className="snake" style={{top:0, left:'2%'}}></div>
+        <div className="snake" style={{top:0, left:'4%'}}></div>
+      </div>
+    );
+  }
 }
 
 export default App;
