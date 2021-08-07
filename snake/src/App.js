dc39686891260f5403479085b70d7a497a508974
alec-deng <alexdeng901@gmail.com>
2021-08-07 17:29:08 -0400

    well done

--- snake/src/App.js
+++ snake/src/App.js
@@ -11,14 +11,67 @@ const randomCoordinate = () => {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      wall: false,
+      playing: false,
+      score: -1,
+    }
+  }
 
-  state = {
-    food: randomCoordinate(),
-    snake: [[0,0],[1,0]],
-    direction: 2, // 0:left, 1:up, 2:right, 3:down
-    speed: 130,  // simulating infinity
-    wall: false,
-    button: "Edges Off"
+  playGame = () => {
+    this.setState({playing: true});
+  }
+  endGame = (score) => {
+    this.setState({playing: false, score: score});
+  }
+
+  switch = () => {
+    this.setState({
+      wall: this.state.wall ? false : true,
+      
+    });
+  }
+
+  render() {
+    return (
+        (this.state.playing) ?
+        <Board wall={this.state.wall} end={this.endGame}/> : 
+        <Interface wall={this.state.wall} play={this.playGame} score={this.state.score} switch={this.switch}/>
+    );
+  }
+}
+
+
+class Interface extends Component {
+
+  render () {
+    return (
+      <div className="board">
+      {(this.props.score === -1)? <></> : <p>Game Over! Your score is {this.props.score}.</p >}
+          Game Mode:<button onClick={this.props.switch} style={{margin: '1vh'}}>{(this.props.wall)? "Edges On" : "Edges Off"}</button>
+          <p className="description">When the edges are off, the snake won't die when it hits the edge of the board,
+                                     instead, it will come out from the other side. When the edges
+                                     are on, the snake will die if it tries to run out of the board.
+                                     <br></br><br></br>
+                                     (use arrow keys to control the snake) </p >
+          <br/><br/><br/>
+          <button onClick={this.props.play}>Play Game</button>      
+      </div>
+    );
+  }
+}
+
+class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      food: randomCoordinate(),
+      snake: [[0,0],[1,0]],
+      direction: 2, // 0:left, 1:up, 2:right, 3:down
+      speed: 130,  // simulating infinity
+    }
   }
 
   componentDidMount = () => {
@@ -37,12 +90,12 @@ class App extends Component {
   }
 
   checkEdges = () => {
-    if ( !(this.state.wall) ) {
+    if ( !(this.props.wall) ) {
       return;
     }
     let head = this.state.snake[this.state.snake.length - 1];
     if ( head[0] >= 20 || head[0] < 0 || head[1] >= 20 || head[1] < 0 ) {
-      if ( this.state.wall ) {
+      if ( this.props.wall ) {
         this.resetGame();
       } else {
         
@@ -91,25 +144,25 @@ class App extends Component {
     let head = snake[snake.length - 1];
     let dir = this.state.direction;
     if ( dir === 0 ) {                            // left
-      if ( this.state.wall === false && head[0] === 0 ) {
+      if ( this.props.wall === false && head[0] === 0 ) {
         head = [19, head[1]];
       } else {
         head = [head[0] - 1, head[1]];
       }
     } else if ( dir === 1 ) {                     // up
-      if ( this.state.wall === false && head[1] === 0 ) {
+      if ( this.props.wall === false && head[1] === 0 ) {
         head = [head[0], 19];
       } else {
         head = [head[0], head[1] - 1];
       }
     } else if ( dir === 2 ) {                     // right
-      if ( this.state.wall === false && head[0] === 19 ) {
+      if ( this.props.wall === false && head[0] === 19 ) {
         head = [0, head[1]];
       } else {
         head = [head[0] + 1, head[1]];
       }
     } else if ( dir === 3 ) {                     // down
-      if ( this.state.wall === false && head[1] === 19 ) {
+      if ( this.props.wall === false && head[1] === 19 ) {
         head = [head[0], 0];
       } else {
         head = [head[0], head[1] + 1];
@@ -122,6 +175,7 @@ class App extends Component {
 
   resetGame = () => {
     alert(`Game Over! Your score is: ${this.state.snake.length - 2}`);
+    this.props.end(this.state.snake.length - 2);
     this.setState({
       food: randomCoordinate(),
       snake: [[0,0],[1,0]],
@@ -129,31 +183,15 @@ class App extends Component {
     });
   }
 
-  swich = () => {
-    this.setState({
-      wall: this.state.wall ? false : true,
-      button: (this.state.button === "Edges Off") ? "Edges On" : "Edges Off"
-    });
-  }
-
-  render() {
+  render () {
     return (
-      <div className="game">
-        <div className="rule">
-          <button className="button" onClick={this.swich}>{this.state.button}</button>
-          <p className="description">When the edges are off, the snake won't die when it hits the edge of the board,
-                                     instead, it will come out from the other side. When the edges
-                                     are on, the snake will die if it tries to run out of the board.
-                                     <br></br><br></br>
-                                     (use arrow keys to control the snake) </p>
-        </div>
-        <div className="board">
+      <div className="board">
           <Snake snake={this.state.snake}/>
           <Food food={this.state.food}/>
-        </div>
       </div>
     );
   }
+
 }
 
 export default App;
